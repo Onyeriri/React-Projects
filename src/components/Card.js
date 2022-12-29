c1d034b64b76ae633cb0ca3cbf5058535e743917
Onyeriri Victor <viktorchinonso@gmail.com>
2022-12-29 07:19:25 +0000

    Created composition component

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+import "./Card.css";
+
+function Card({ children, className }) {
+  const classes = `card ${className}`;
+  return <div className={classes}>{children}</div>;
+}
+
+export default Card;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem({ expense }) {
   const { date, amount, title } = expense;
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
